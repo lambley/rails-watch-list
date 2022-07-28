d989613e87d23dc22cef37519c4c3de8109b0881
Aaron Lambley <aaron.lambley@gmail.com>
2022-07-28 20:04:22 +0100

    add functionality to add/edit cover list photos add edit route for lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,5 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: %i[show destroy]
+  before_action :set_list, only: %i[show destroy edit update]
 
   def index
     @lists = List.all
@@ -22,6 +22,18 @@ class ListsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @list.update(list_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      redirect_to edit_list_path(@list), status: :unprocessable_entity
+    end
+  end
+
   def destroy
     @list.destroy
     redirect_to lists_path, status: :see_other
@@ -34,6 +46,6 @@ class ListsController < ApplicationController
   end
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
